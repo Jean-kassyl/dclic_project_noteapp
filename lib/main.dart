d03d09c478f4be0f2d64ad10fcc10706f786e147
Jean-kassyl <kassylmac@gmail.com>
2025-09-04 12:03:13 +0100

    create the login page and make it look like my wireframe

--- lib/main.dart
+++ lib/main.dart
@@ -35,19 +35,39 @@ class _LoginPageState extends State<LoginPage> {
       body: Container(
         padding: EdgeInsets.all(20),
         child: Column(
+          
+          spacing: 25,
           children: [
             Text(
               "My notes",
+              
               style: TextStyle(
-                color: Colors.grey[800],
+               
+                color: Colors.grey[720],
+                fontSize: 40,
+                letterSpacing: 1.2,
+                
+                fontWeight: FontWeight.bold,
               ),
             ),
-            Image.asset(
-                "assets/images/noteImg.jpeg"
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Image.asset(
+                "assets/images/noteImg.jpeg",
+                fit: BoxFit.contain,
+                color: Colors.yellow,
+                colorBlendMode: BlendMode.softLight,
+                
+            ),
             ),
             Text(
-              "Welcome to this application",
+              "Bienvenue sur l'appli my notes. Renseigner le formulaire suivant pour accéder à l'application. Un compte automatique sera créer dès que vous le remplisser une première fois. Vous n'avez droit qu'a 5 compte sur l'appli. Amusez vous bien!",
               maxLines: null,
+              style: TextStyle(
+                color: Colors.grey[450],
+                fontSize: 14,
+                wordSpacing: 0.4
+              ),
             ),
             TextField(
               controller: _usernameController,
@@ -59,6 +79,7 @@ class _LoginPageState extends State<LoginPage> {
 
             TextField(
               controller: _passwordController,
+              obscureText: true,
               decoration: InputDecoration(
                 labelText: "Mot de passe",
                 border: UnderlineInputBorder(),
@@ -66,8 +87,14 @@ class _LoginPageState extends State<LoginPage> {
             ),
             ElevatedButton(
               onPressed: (){},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.yellow,
+                fixedSize: Size(320, 40),
+                elevation: 0,
+              ),
               child: Text(
-                "Login"
+                "S'enregistrer",
+                style: TextStyle(fontSize: 15, color: Colors.grey[140])
               ),
             ),
           ],
